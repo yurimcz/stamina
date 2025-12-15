228ba075dd14e25df9278ce29e83b7e889853308
yurimcz <yurinho321@hotmail.com>
2025-12-15 07:47:42 -0300

    initial upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'stamina-pwa-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
